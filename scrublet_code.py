1a3832fb490a2787968869c1c72ae6f9383dd914
fenisha29 <147111562+fenisha29@users.noreply.github.com>
2023-10-15 12:08:13 -0400

    Update scrublet_code.py

--- scrublet_code.py
+++ scrublet_code.py
@@ -16,8 +16,10 @@ import scrublet as scr
 import scipy.io
 import sys
 
-patient=sys.argv[1]
-doublet_rate=float(sys.argv[2])
+#patient=sys.argv[1]
+patient = "RU1065C_MET_LI"
+#doublet_rate=float(sys.argv[2])
+doublet_rate= 0.0644
 
 print('Patient:', patient, 'Doublet_rate:', doublet_rate)
 print(sys.argv)
