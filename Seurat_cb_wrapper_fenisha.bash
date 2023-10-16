8bd0ffdc2a596c0fafa4753e476b87c0607f5de5
fenisha29 <147111562+fenisha29@users.noreply.github.com>
2023-10-16 12:40:47 -0400

    Update Seurat_cb_wrapper_fenisha.bash

--- Seurat_cb_wrapper_fenisha.bash
+++ Seurat_cb_wrapper_fenisha.bash
@@ -2,7 +2,7 @@
 
 
 declare -a samples
-samples=(sample1 sample2 sample3)
+samples=(RU1066_PRI_LU, RU1080C_MET_KI, RU1108a_REC_LU_b, RU1108a_REC_LU_bf, RU1108a_REC_LU_rpmi, RU1124A_MET_LN, RU1144_MET_LN, RU1144_REC_LU, RU1145_PRI_LU, RU1152_MET_LN)
 
 for i in ${samples[@]}; do
   nohup Seurat_cb_doubletfinder.R $i > cb_${i}.out 2>&1 &
