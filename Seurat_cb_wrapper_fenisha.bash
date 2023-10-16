4c7afde8949106f9b968b83caf116f8a7109ab4c
fenisha29 <147111562+fenisha29@users.noreply.github.com>
2023-10-16 12:35:12 -0400

    Update Seurat_cb_wrapper_fenisha.bash

--- Seurat_cb_wrapper_fenisha.bash
+++ Seurat_cb_wrapper_fenisha.bash
@@ -5,5 +5,5 @@ declare -a samples
 samples=(sample1 sample2 sample3)
 
 for i in ${samples[@]}; do
-  nohup brain_mets/sclc/Seurat/Seurat_cb_doubletfinder.R $i > cb_${i}.out 2>&1 &
+  nohup Seurat_cb_doubletfinder.R $i > cb_${i}.out 2>&1 &
 done
