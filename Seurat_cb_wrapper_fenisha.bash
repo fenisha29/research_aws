59d4f5a06d802071e16c4e393ee683c1aa13a1b6
fenisha29 <147111562+fenisha29@users.noreply.github.com>
2023-10-16 12:33:33 -0400

    Add files via upload

---
+++ Seurat_cb_wrapper_fenisha.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+declare -a samples
+samples=(sample1 sample2 sample3)
+
+for i in ${samples[@]}; do
+  nohup brain_mets/sclc/Seurat/Seurat_cb_doubletfinder.R $i > cb_${i}.out 2>&1 &
+done
