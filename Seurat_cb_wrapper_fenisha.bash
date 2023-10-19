fbf4ae2b2d8cccfc07c8a96c29bc95d1cc89968a
fenisha29 <147111562+fenisha29@users.noreply.github.com>
2023-10-19 11:25:43 -0400

    Update Seurat_cb_wrapper_fenisha.bash

--- Seurat_cb_wrapper_fenisha.bash
+++ Seurat_cb_wrapper_fenisha.bash
@@ -2,8 +2,9 @@
 
 
 declare -a samples
-samples=(RU1066_PRI_LU RU1080C_MET_KI RU1108a_REC_LU_b RU1108a_REC_LU_bf RU1108a_REC_LU_rpmi RU1124A_MET_LN RU1144_MET_LN RU1144_REC_LU RU1145_PRI_LU RU1152_MET_LN)
+samples=(RU1066_PRI_LU)
+#RU1080C_MET_KI RU1108a_REC_LU_b RU1108a_REC_LU_bf RU1108a_REC_LU_rpmi RU1124A_MET_LN RU1144_MET_LN RU1144_REC_LU RU1145_PRI_LU RU1152_MET_LN)
 
 for i in ${samples[@]}; do
-  nohup ./Seurat_cb_doubletfinder.R $i > cb_${i}.out 2>&1 &
+  nohup brain_mets/sclc/Seurat/Seurat_cb_doubletfinder.R $i > cb_${i}.out 2>&1 &
 done
