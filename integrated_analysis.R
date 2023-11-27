a75d66fed711727fca0fd3a22f48cf9846e07374
fenisha29 <147111562+fenisha29@users.noreply.github.com>
2023-11-27 17:05:53 -0500

    Update integrated_analysis.R

--- integrated_analysis.R
+++ integrated_analysis.R
@@ -7,9 +7,23 @@ summary(integrated_data)
 
 unique_cell_types <- unique(integrated_data$celltype_bped_main)
 
+# Run UMAP
+integrated_data <- RunUMAP(integrated_data, dims = 1:30)
 
-pdf("DimPlot_output.pdf")
+# Save UMAP plot to a PDF file
+pdf("UMAP_plot.pdf")
+
 # Use DimPlot with group.by set to 'celltype_bped_main' and labels from unique_cell_types
 DimPlot(integrated_data, label = TRUE, group.by = "celltype_bped_main", raster = TRUE, shuffle = TRUE)
+
+# Use DimPlot with group.by set to 'sample'
 DimPlot(integrated_data, label = TRUE, group.by = "sample", raster = TRUE, shuffle = TRUE)
+
+# Close the PDF device
 dev.off()
+
+#pdf("DimPlot_output.pdf")
+# Use DimPlot with group.by set to 'celltype_bped_main' and labels from unique_cell_types
+#DimPlot(integrated_data, label = TRUE, group.by = "celltype_bped_main", raster = TRUE, shuffle = TRUE)
+#DimPlot(integrated_data, label = TRUE, group.by = "sample", raster = TRUE, shuffle = TRUE)
+#dev.off()
