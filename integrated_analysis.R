a6f51ab42ebde21e3568f422acd6386d681fa850
fenisha29 <147111562+fenisha29@users.noreply.github.com>
2023-11-27 16:50:06 -0500

    Update integrated_analysis.R

--- integrated_analysis.R
+++ integrated_analysis.R
@@ -3,5 +3,11 @@ file_path_1 <- "vol/integrated_seurat_object.rds"
 integrated_data <- readRDS(file_path_1)
 head(integrated_data)
 summary(integrated_data)
-names <- names(integrated_data@meta.data)
-DimPlot(integrated_data, label = T,group.by = names ,raster = T,shuffle = T)
+#names <- names(integrated_data@meta.data)
+
+unique_cell_types <- unique(integrated_data$celltype_bped_main)
+
+pdf("DimPlot_output.pdf")
+# Use DimPlot with group.by set to 'celltype_bped_main' and labels from unique_cell_types
+DimPlot(integrated_data, label = TRUE, group.by = "celltype_bped_main", raster = TRUE, shuffle = TRUE)
+dev.off()
