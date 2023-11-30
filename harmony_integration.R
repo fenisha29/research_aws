0f6d63e4c73a2286baf00dbb0341fcf1096a57d7
fenisha29 <147111562+fenisha29@users.noreply.github.com>
2023-11-29 19:19:08 -0500

    Update harmony_integration.R

--- harmony_integration.R
+++ harmony_integration.R
@@ -29,29 +29,43 @@ seurat_files <- c(
 #seu <- lapply(seurat_files, function(file_path) {
 #  readRDS(file_path)
 #})
-seu <- lapply(seurat_files, readRDS)
-DefaultAssay(seu) <- 'RNA'
-seu <- DietSeurat(seu, assays = 'RNA')
-seu <- NormalizeData(seu) %>%
-  FindVariableFeatures() %>%
-  ScaleData() %>%
-  RunPCA(npcs = 50)
+# Load Seurat objects
+seu_list <- lapply(seurat_files, readRDS)
 
-seu <- RunHarmony(object = seu, group.by.vars = 'sample', dims.use = 1:30,
-                  assay.use = 'RNA', plot_convergence = TRUE)
-seu <- RunUMAP(seu, reduction = 'harmony', dims = 1:30)
+# Apply DefaultAssay to each Seurat object in the list
+seu_list <- lapply(seu_list, function(seu) {
+  DefaultAssay(seu) <- 'RNA'
+  return(seu)
+})
 
-# Assuming you have already run Harmony and UMAP on your Seurat object
-seu <- RunUMAP(seu, reduction = 'harmony', dims = 1:30)
+# Run other pre-processing steps on each Seurat object
+seu_list <- lapply(seu_list, function(seu) {
+  seu <- DietSeurat(seu, assays = 'RNA')
+  seu <- NormalizeData(seu) %>%
+    FindVariableFeatures() %>%
+    ScaleData() %>%
+    RunPCA(npcs = 50)
+  return(seu)
+})
+
+# Run Harmony on the list of Seurat objects
+seu_list <- RunHarmony(object = seu_list, group.by.vars = 'sample', dims.use = 1:30,
+                        assay.use = 'RNA', plot_convergence = TRUE)
+
+# Run UMAP on each Seurat object in the list
+seu_list <- lapply(seu_list, function(seu) {
+  seu <- RunUMAP(seu, reduction = 'harmony', dims = 1:30)
+  return(seu)
+})
 
 # Create a PDF file to save the plots
 pdf("UMAP_after_Harmony.pdf")
 
 # Plot UMAP with labels
-DimPlot(seu, group.by = "celltype_bped_main", label = TRUE)
+DimPlot(seu_list[[1]], group.by = "celltype_bped_main", label = TRUE)
 
 # Add more plots if needed, e.g., by sample
-DimPlot(seu, group.by = "sample", label = TRUE)
+DimPlot(seu_list[[1]], group.by = "sample", label = TRUE)
 
 # Close the PDF file
 dev.off()
